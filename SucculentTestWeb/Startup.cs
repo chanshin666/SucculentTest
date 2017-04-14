0a2e36c7b0da2d0fd9f1415eaa672b3744fa775f
chanshin666 <763225207@qq.com>
2017-04-14 17:41:11 +0800

    多肉测试项目

---
+++ SucculentTestWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SucculentTestWeb.Startup))]
+namespace SucculentTestWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
